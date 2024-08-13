41f289a742265469ce107933a68c7daa498f7a46
nova <oliverquirk11@gmail.com>
2024-08-13 21:31:02 +0100

    V0.035 (Formatting)
    Update Notes:
    
    -Formatting updates.
    
    Pending Tasks:
    
    -None.
    
    To-do:
    -Move track functionality from track.html to overwatchTracker.html.
    -Add socials to the socials page.
    -Fix side menu click on phones when clicking the menu icon for the current page.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,10 @@ const closeButton = document.getElementById("closeButton")
 const sideMenu = document.getElementById("sideMenu");
 
 document.addEventListener("click", (event) =>{
+    console.log("Clicked element:", event.target);
     if(hamburgerMenu.contains(event.target)|| closeButton.contains(event.target))
     {
+        console.log("Toggling side menu");
         sideMenu.classList.toggle("open");
     }
 });
